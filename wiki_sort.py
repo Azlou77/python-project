b2f95bb6993cee5f4cded56f8d2c505d8939cffa
Nguyen Louis <nguyenlouis172@gmail.com>
2023-06-13 09:33:34 +0200

    add wiki2 exercise

---
+++ wiki_sort.py
@@ -0,0 +1,3 @@
+texteWiki ="La Fondation Wikimédia, organisation à but non lucratif américaine sise à San Francisco, est dépositaire de la marque Wikipedia. Elle finance l'hébergement web de l'encyclopédie. " 
+
+# Sort the numbers with texte_tri
