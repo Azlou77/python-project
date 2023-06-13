2f511c2a69549f0168aa9492cc42edcc50038865
Nguyen Louis <nguyenlouis172@gmail.com>
2023-06-13 09:33:09 +0200

    add dico exercise

---
+++ dico.py
@@ -0,0 +1,8 @@
+# Initialize a dictionary
+Dico_nature = {"Canidés" : ["Loup","Dingo",{'Chien':["Chihuahua","Fox Retriever"]}],
+               "Chat": "Persan"}
+Dico_nature
+
+# Display the number of races of canides 
+number_of_canides = len(Dico_nature["Canidés"])
+print(f"Number of canides: {number_of_canides}")
