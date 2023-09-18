7e50f9067732673b23f794766f2335a1487fa0b4
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-09-18 18:40:56 +0000

    add first array exercise

---
+++ exercises/array/array1.py
@@ -0,0 +1,12 @@
+"""  Write a Python program to create an array of 5 integers 
+     and display the array items. Access individual elements through indexes. 
+ """
+from array import *
+# 1. Create an array of 5 integers
+# i means signed integer
+arrayNum = array('i', [1, 3, 5, 7, 9])
+
+# 2. Display the array items
+print(arrayNum[0])
+print(arrayNum[1])
+print(arrayNum[2])
